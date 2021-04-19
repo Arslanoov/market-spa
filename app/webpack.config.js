85d68f740ffe4795259b6ab63dcdc8162beef7af
Arslanoov <rasularslanoov@gmail.com>
2021-04-19 19:42:25 +0500

    build: Installed React app

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,1 +1,29 @@
-module.exports = {};
+const path = require("path")
+
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+
+  output: {
+    path: path.resolve(__dirname, "dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
