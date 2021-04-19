6f66cea664c952ba6953d01b5862870ecd4befa6
Arslanoov <rasularslanoov@gmail.com>
2021-04-19 20:03:48 +0500

    build: Added ts

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import * as React from "react"
 
 import "./app.scss"
 
-const App = () => (
+const App: React.FC = () => (
   <div className="text">Some app</div>
 )
 
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -5,20 +5,20 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", "index.jsx"),
+  entry: path.resolve(__dirname, "src", "index.tsx"),
 
   output: {
     path: path.resolve(__dirname, "dist")
   },
 
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
   },
 
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader"
       },
