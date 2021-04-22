56f8c041fad30c47aacb6c3318884990292a0447
Arslanoov <rasularslanoov@gmail.com>
2021-04-22 19:28:26 +0500

    feat: Added layout grid

--- app/src/components/Counter.tsx
+++ app/src/components/Counter.tsx
@@ -6,7 +6,7 @@ import CounterStore from "~/stores/Counter"
 
 const Counter: React.FC = observer(() => {
   return (
-    <div>
+    <div className="container">
       <div>counter: {CounterStore.count}</div>
       <div onClick={() => CounterStore.increment()}>+</div>
       <div onClick={() => CounterStore.decrement()}>-</div>
