4d417f03b94505207f5893dc3944d16269083d61
Arslanoov <rasularslanoov@gmail.com>
2021-04-23 20:53:02 +0500

    feat: Welcome section

--- app/src/components/common/welcome/Welcome.tsx
+++ app/src/components/common/welcome/Welcome.tsx
@@ -1,5 +1,8 @@
 import * as React from "react"
 
+import Image from "~/assets/images/welcome/image.jpg"
+import Sign from "~/assets/images/welcome/sign.png"
+
 import "./styles.scss"
 
 const Welcome: React.FC = () => {
@@ -7,9 +10,25 @@ const Welcome: React.FC = () => {
     <div className="container welcome">
       <div className="welcome__title">s.u.m.o</div>
       <div className="welcome__content">
-        Content
-        <div className="welcome__image">Image</div>
+        <h2 className="welcome__story">Our story</h2>
+        <div className="welcome__text">
+          <p className="welcome__paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry&#39;s standard dummy text ever since the 1500s.
+          </p>
+          <p className="welcome__paragraph">
+            When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived
+            not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          </p>
+          <p className="welcome__paragraph">
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
+            more recently with desktop publishing software like Aldus
+          </p>
+        </div>
+        <img className="welcome__sign" src={Sign} alt="" />
+        <div className="welcome__author">Mr. Barak - CEO s.u.m.o</div>
       </div>
+      <img className="welcome__image" src={Image} alt="" />
     </div>
   )
 }
