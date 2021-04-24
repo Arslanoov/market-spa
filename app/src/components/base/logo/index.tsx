c2e6e0c8517f472af3575713b21decfb305e0b9b
Arslanoov <rasularslanoov@gmail.com>
2021-04-24 08:02:22 +0500

    feat: Added logo title dots

--- app/src/components/base/logo/index.tsx
+++ app/src/components/base/logo/index.tsx
@@ -5,7 +5,9 @@ import "./styles.scss"
 const Logo: React.FC = () => {
   return (
     <div className="logo">
-      <h1 className="logo__title">s.u.m.o</h1>
+      <h1 className="logo__title">
+        s<span className="logo__dot">.</span>u<span className="logo__dot">.</span>m<span className="logo__dot">.</span>o
+      </h1>
       <h2 className="logo__subtitle">watch</h2>
     </div>
   )
