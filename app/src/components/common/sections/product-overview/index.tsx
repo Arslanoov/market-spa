57ddd943955ca2303026fb313ae0e03da775bb53
Arslanoov <rasularslanoov@gmail.com>
2021-04-24 14:20:32 +0500

    feat: Added product overview section

--- app/src/components/common/sections/product-overview/index.tsx
+++ app/src/components/common/sections/product-overview/index.tsx
@@ -6,9 +6,28 @@ import "./styles.scss"
 
 const ProductOverview: React.FC = () => {
   return (
-    <div className="product-overview">
-      <div className="product-overview__content">Omega Planet Ocean Chronograph</div>
-      <Overview />
+    <div className="product-overview container">
+      <div className="product-overview__wrapper">
+        <div className="product-overview__content">
+          <h2 className="product-overview__title">Omega Planet Ocean Chronograph</h2>
+          <p className="product-overview__description product-overview__description_important">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis, est id mattis tempus, magna ligula
+            facilisis lorem, nec dapibus turpis leo ac est.
+          </p>
+          <p className="product-overview__description">
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus
+            elementum ex id justo molestie, rhoncus blandit metus interdum. Nullam laoreet nisi tellus, et luctus justo.
+          </p>
+          <p className="product-overview__description">
+            Vivamus elementum ex id justo molestie, rhoncus blandit metus interdum. Nullam laoreet nisi tellus, et
+            luctus justo.
+          </p>
+          <button className="product-overview__detail">View detail</button>
+        </div>
+        <div className="product-overview__overview">
+          <Overview />
+        </div>
+      </div>
     </div>
   )
 }
