36a566037946078757bde5602e812b4fefdced1b
Arslanoov <rasularslanoov@gmail.com>
2021-04-25 09:36:56 +0500

    feat: Added select filter

--- app/src/components/common/filters/row/index.tsx
+++ app/src/components/common/filters/row/index.tsx
@@ -1,15 +1,62 @@
 import * as React from "react"
 
 import ViewFilter from "~/components/common/filters/view"
-import SortFilter from "~/components/common/filters/sort"
+import SelectFilter from "~/components/common/filters/select"
 
 import "./styles.scss"
 
 const FiltersRow: React.FC = () => {
   return (
     <div className="filters-row">
-      <ViewFilter />
-      <SortFilter />
+      <div className="filters-row__item">
+        <ViewFilter />
+      </div>
+
+      <div className="filters-row__item">
+        <div className="filters-row__prefix">Sort by</div>
+        <SelectFilter
+          minWidth="20rem"
+          filters={[
+            {
+              id: 1,
+              value: "Default sorting",
+            },
+            {
+              id: 2,
+              value: "Latest",
+            },
+            {
+              id: 3,
+              value: "Most popular",
+            },
+          ]}
+        />
+      </div>
+
+      <div className="filters-row__item">
+        <div className="filters-row__prefix">Show</div>
+        <SelectFilter
+          minWidth="7.6rem"
+          filters={[
+            {
+              id: 1,
+              value: 16,
+            },
+            {
+              id: 2,
+              value: 12,
+            },
+            {
+              id: 3,
+              value: 10,
+            },
+            {
+              id: 4,
+              value: 5,
+            },
+          ]}
+        />
+      </div>
     </div>
   )
 }
